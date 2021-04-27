269b0e77348f273e66149cd5283ae6a00c4c41d5
baebaeseonhuii <baeseonhui3@gmail.com>
2021-04-27 19:04:34 +0900

    made controller files left

---
+++ FinalProject/ViewControllers/CalendarViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarViewController.swift
+//  FinalProject
+//
+//  Created by Sun hee Bae on 2021/04/27.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FinalProject/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsViewController.swift
+//  FinalProject
+//
+//  Created by Sun hee Bae on 2021/04/27.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
