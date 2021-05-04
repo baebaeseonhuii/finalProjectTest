ceeacc5a5e1240d9615b3d6a0f3d58bfef75b6be
baebaeseonhuii <baeseonhui3@gmail.com>
2021-05-04 20:13:28 +0900

    additional commit

--- FinalProject/ViewControllers/InsideCalendarViewController.swift
+++ FinalProject/ViewControllers/InsideCalendarViewController.swift
@@ -6,8 +6,26 @@
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
-class InsideCalendarViewController: UIViewController {
+class InsideCalendarViewController: UIViewController, SFSpeechRecognizerDelegate {
+    
+    @IBOutlet weak var myTxt: UILabel!
+    let synthesizer = AVSpeechSynthesizer()
+
+    let utterance = AVSpeechUtterance(string: "your string")
+
+    utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+    
+    utterance.rate = 0.4
+
+    synthesizer.speak(utterance)
+
+
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
