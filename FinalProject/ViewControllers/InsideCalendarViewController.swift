1a8e1bd2e06e9845b76075c604d94b68b6530c49
baebaeseonhuii <baeseonhui3@gmail.com>
2021-05-18 00:28:24 +0900

    generating app icon

--- FinalProject/ViewControllers/InsideCalendarViewController.swift
+++ FinalProject/ViewControllers/InsideCalendarViewController.swift
@@ -10,22 +10,6 @@ import Speech
 import AVFoundation
 
 class InsideCalendarViewController: UIViewController, SFSpeechRecognizerDelegate {
-    
-    @IBOutlet weak var myTxt: UILabel!
-    let synthesizer = AVSpeechSynthesizer()
-
-    let utterance = AVSpeechUtterance(string: "your string")
-
-    utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
-    
-    utterance.rate = 0.4
-
-    synthesizer.speak(utterance)
-
-
-
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
