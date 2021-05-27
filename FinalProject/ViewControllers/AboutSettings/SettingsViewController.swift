79871d7c9057494258cf95768e1ec10aea54389f
baebaeseonhuii <baeseonhui3@gmail.com>
2021-05-27 23:23:16 +0900

    5/27 setting tap

--- FinalProject/ViewControllers/AboutSettings/SettingsViewController.swift
+++ FinalProject/ViewControllers/AboutSettings/SettingsViewController.swift
@@ -9,7 +9,20 @@ import UIKit
 
 struct Section {
     let title: String
-    let options: [SettingsOption]
+    let options: [SettingsOptionType]
+}
+
+enum SettingsOptionType {
+    case staticCell(model: SettingsOption)
+    case switchCell(model: SettingsSwitchOption)
+}
+
+struct SettingsSwitchOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: (() -> Void)
+    var isOn: Bool
 }
 
 struct SettingsOption {
@@ -25,10 +38,13 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         let table = UITableView(frame: .zero, style: .grouped)
         table.register(SettingTableViewCell.self,
                        forCellReuseIdentifier: SettingTableViewCell.identifier)
+        table.register(SwitchTableViewCell.self,
+                       forCellReuseIdentifier: SwitchTableViewCell.identifier)
+        
         return table
     } ()
     
-    var models = [SettingsOption]()
+    var models = [Section]()
     
     
     override func viewDidLoad() {
@@ -42,43 +58,94 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func configure() {
-        self.models = Array(0...5).compactMap({
-            SettingsOption(title: "Item \($0)", icon: UIImage(systemName: "house"), iconBackgroundColor: .systemPink) {
+        models.append(Section(title: "음성 안내 설정", options: [
+            .switchCell(model: SettingsSwitchOption(title: "인식 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "speaker.wave.2"), iconBackgroundColor: .systemIndigo, handler: {
                 
-            }
-        })
+            }, isOn: true)),
+            
+        ]))
+        
+        models.append(Section(title: "알림 설정", options: [
+            .staticCell(model: SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "speaker.wave.2"), iconBackgroundColor: .systemPink) {
+                print("Tapped first cell")
+            }),
+            .staticCell(model: SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "speaker.wave.2"), iconBackgroundColor: .systemGreen) {
+                print("Tapped first cell")
+            }),
+            .staticCell(model: SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "speaker.wave.2"), iconBackgroundColor: .systemIndigo) {
+                print("Tapped first cell")
+            }),
+            .staticCell(model: SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "speaker.wave.2"), iconBackgroundColor: .systemYellow) {
+                print("Tapped first cell")
+            })
+
+
+        ]))
+        
+        models.append(Section(title: "기타 설정", options: [
+            .staticCell(model: SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "speaker.wave.2"), iconBackgroundColor: .systemPink) {
+                print("Tapped first cell")
+            }),
+            .staticCell(model: SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "speaker.wave.2"), iconBackgroundColor: .systemPink) {
+                print("Tapped first cell")
+            }),
+            .staticCell(model: SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "speaker.wave.2"), iconBackgroundColor: .systemPink) {
+                print("Tapped first cell")
+            }),
+            .staticCell(model: SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "speaker.wave.2"), iconBackgroundColor: .systemPink) {
+                print("Tapped first cell")
+            })
+
+
+        ]))
     }
     
-//    func configure() {
-//        models.append(Section(title: "음성 안내", options: [
-//            SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "mic"), iconBackgroundColor: .systemPink) {
-//
-//            },
-//            SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "mic"), iconBackgroundColor: .systemPink) {
-//
-//            },
-//            SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "mic"), iconBackgroundColor: .systemPink) {
-//
-//            },
-//            SettingsOption(title: "안내 결과를 음성으로 알려줍니다.", icon: UIImage(systemName: "mic"), iconBackgroundColor: .systemPink) {
-//
-//            },
-//
-//        ]))
-//    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let section = models[section]
+        return section.title
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return models.count
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return models.count
+        return models[section].options.count
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let model = models[indexPath.row]
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath
-        ) as? SettingTableViewCell else {
-            return UITableViewCell()
+        let model = models[indexPath.section].options[indexPath.row]
+        
+        switch model.self {
+        case .staticCell(let model):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath
+            ) as? SettingTableViewCell else {
+                return UITableViewCell()
+            }
+            cell.configure(with: model)
+            return cell
+            
+        case .switchCell(let model):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SwitchTableViewCell.identifier, for: indexPath
+            ) as? SwitchTableViewCell
+            else {
+                return UITableViewCell()
+            }
+            cell.configure(with: model)
+            return cell
         }
-        cell.configure(with: model)
-        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let type = models[indexPath.row].options[indexPath.row]
+        switch type.self {
+        case .staticCell(let model):
+            model.handler()
+        case .switchCell(let model):
+            model.handler()
+        }
     }
 }
